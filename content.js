26b61d91f876859e5196dea6fd6cc2450c71c49c
Maksym Hoch <maksymhoch@gmail.com>
2024-11-02 00:25:26 +0000

    firtsfd

--- content.js
+++ content.js
@@ -47,11 +47,12 @@ let car = {
   MaxSpeed: 250,
 
   accelerate: function (SpeedPerHour) {
-           
-      if (!Number.isInteger(SpeedPerHour) || SpeedPerHour < 0) {
-        console.log("Будь ласка, введіть ціле додатне число для збільшення швидкості.");
-        return; 
-      }
+    if (!Number.isInteger(SpeedPerHour) || SpeedPerHour < 0) {
+      console.log(
+        "Будь ласка, введіть ціле додатне число для збільшення швидкості."
+      );
+      return;
+    }
 
     if (this.speed + SpeedPerHour > 250) {
       this.speed = this.MaxSpeed;
@@ -60,23 +61,83 @@ let car = {
       this.speed += SpeedPerHour;
       console.log("Швидкість" + this.speed);
     }
-  }
+  },
 
-  deaccelerate: function(slowDown) {
-           
+  deaccelerate: function (slowDown) {
     if (!Number.isInteger(slowDown) || slowDown < 0) {
-      console.log("Будь ласка, введіть ціле додатне число для збільшення швидкості.");
-      return; 
+      console.log(
+        "Будь ласка, введіть ціле додатне число для збільшення швидкості."
+      );
+      return;
     }
 
-  if (this.speed - slowDown < 0) {
-    console.log("Машина зупинилась");
-  } else {
-    this.speed -= slowDown;
-    console.log("Нова швидкість" + this.speed);
-  }
+    if (this.speed - slowDown < 0) {
+      console.log("Машина зупинилась");
+    } else {
+      this.speed -= slowDown;
+      console.log("Нова швидкість" + this.speed);
+    }
+  },
+
+  stop: function () {
+    this.speed = 0;
+    console.log("Машина зупинилась.");
+  },
+};
+
+
+
+//task 5
+
+function Car (color, model, brand, engine, seats, speed, MaxSpeed) {
+ this.color = color;
+  this.model = model;
+  this.brand = brand;
+  this.engine = engine;
+  this.seats = seats;
+  this.speed = speed;
+  this.MaxSpeed = MaxSpeed;
+
+  accelerate: function (SpeedPerHour) {
+    if (!Number.isInteger(SpeedPerHour) || SpeedPerHour < 0) {
+      console.log(
+        "Будь ласка, введіть ціле додатне число для збільшення швидкості."
+      );
+      return;
+    }
+
+    if (this.speed + SpeedPerHour > 250) {
+      this.speed = this.MaxSpeed;
+      console.log("Швидкість підвищена" + this.MaxSpeed);
+    } else {
+      this.speed += SpeedPerHour;
+      console.log("Швидкість" + this.speed);
+    }
+  },
+
+  deaccelerate: function (slowDown) {
+    if (!Number.isInteger(slowDown) || slowDown < 0) {
+      console.log(
+        "Будь ласка, введіть ціле додатне число для збільшення швидкості."
+      );
+      return;
+    }
+
+    if (this.speed - slowDown < 0) {
+      console.log("Машина зупинилась");
+    } else {
+      this.speed -= slowDown;
+      console.log("Нова швидкість" + this.speed);
+    }
+  },
+
+  stop: function () {
+    this.speed = 0;
+    console.log("Машина зупинилась.");
+  },
+
+  let car1 = new Car("red", "Model S", "Tesla", 2.5, 5, 200);
+  let car2 = new Car("blue", "Mustang", "Ford", 4.0, 4, 250);
+  let car3 = new Car("black", "Civic", "Honda", 1.8, 5, 180);
+
 }
-
-
-}
-
